bb7e6e5b942cd473618cf212fd4c657dfbba6711
zwiebert <bertw@online.de>
2020-02-29 00:11:48 +0100

    winio deleted

--- src/WinIo/WinIo-Test/prebuild.bat
+++
@@ -1,5 +0,0 @@
-set config=%1
-
-rem copy /b ..\..\..\..\WinIo\Source\Dll\%config%\WinIO.DLL .\%config%\WinIO.DLL
-rem copy /b ..\..\..\..\WinIo\Source\Drv\NT\I386\WinIO.sys .\%config%\WinIO.sys
-
--- src/WinIo/copy_output.bat
+++
@@ -1,4 +0,0 @@
-rem set directories where this project copies it ouput to at build time and removes it at cleaning time
-set target_paths=..\WinIO-Test\%config% ..\..\..\radeon_setclk\clocker\%config%
- 
-echo Using target directories:  %target_paths%
